2a25a1f9f931b2b3e9124993cfd86a131cb05b1f
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-12-11 12:54:09 +0530

    Updated fully

--- configfile/config.py
+++ configfile/config.py
@@ -18,54 +18,7 @@ def setup_mongodb():
     # Set up test data (example)
     db = client.get_database(DATABASE_NAME)  # Use your actual database
     collection = db.get_collection(USER_COLLECTION)  # Use your collection
-  # Example: Insert some test documents
-    sample_data = [
-       {
-            "username": "",
-            "first_name": "admin",
-            "last_name": "admin",
-            "password": "demo",
-            "mode_2fa": "Off",
-            "groups": ["Admin"],
-            "rights": "Admin",
-            "notes": {"info": "this 'notes' field exists only for this default admin user", "p": "donttrustyou"},
-            "vec_2fa": None,  # Corrected null to None
-            "baseurl": "https://demo.filebrowser.org/login?redirect=/files/",
-            "is_valid": False,
-            "expected_error": "success",
-        },
-        
-        {
-            "username": "demo",
-            "first_name": "admin",
-            "last_name": "admin",
-            "password": "",
-            "mode_2fa": "Off",
-            "groups": ["Admin"],
-            "rights": "Admin",
-            "notes": {"info": "this 'notes' field exists only for this default admin user", "p": "donttrustyou"},
-            "vec_2fa": None,  # Corrected null to None
-            "baseurl": "https://demo.filebrowser.org/login?redirect=/files/",
-            "is_valid": False,
-            "expected_error": "Wrong credentials",
-        },
-        {
-            "username": "demo",
-            "first_name": "admin",
-            "last_name": "admin",
-            "password": "demo",
-            "mode_2fa": "Off",
-            "groups": ["Admin"],
-            "rights": "Admin",
-            "notes": {
-                "info": "this 'notes' field exists only for this default admin user",
-                "p": "donttrustyou"
-            },
-            "vec_2fa": None,  # Corrected null to None
-            "baseurl": "https://demo.filebrowser.org/login?redirect=/files/",
-            "is_valid": True,
-            "expected_error": "success",
-        }
+  
 
 
 
@@ -75,9 +28,6 @@ def setup_mongodb():
 
 
 
-
-    ]
-    collection.insert_many(sample_data)
-    print(f"Test data inserted into {db.name}.{collection.name}")
+  
 if __name__ == "__main__":
     setup_mongodb()
